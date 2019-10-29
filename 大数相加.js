042ad096b0088792a5ebdb6f25b8a1fde32741c5
zeroone001 <ilylys@163.com>
2019-10-29 22:15:26 +0800

    update

--- 大数相加.js
+++ 大数相加.js
@@ -1,1 +1,20 @@
-console.log(1)
+function addTwoNum (str1, str2) {
+    var carry = 0;
+    var l1 = str1.length;
+    var l2 = str2.length;
+    var arr = [];
+    var max = Math.max(l1, l2);
+    var i = l1 - 1, j = l2 - 1;
+    for (var n = max - 1; n >= 0; n--,i--,j-- ) {
+        var m = (+str1[i] || 0) + (+str2[j] || 0) + carry;
+        if (m >= 10) {
+            carry = 1;
+            m = m - 10;
+        } else {
+            carry = 0;
+        }
+        arr.push(m);
+    }
+    if (carry > 0) arr.push(carry);
+    return arr.reverse().join('');
+}
